f2b879af16f0bfc790f2a7f4356eacb219207cbc
Abain-1 <abain.1x@gmail.com>
2025-08-05 19:10:05 -0400

    added clock

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    Clock = pygame.time.Clock()
+
+    dt = Clock.tick(60) / 1000
+
+    Clock.tick(60)
+
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -15,7 +21,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        
+        screen.fill("black")
     
         pygame.display.flip()
 
